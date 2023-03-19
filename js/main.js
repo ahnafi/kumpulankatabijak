62ec003374830666b50816f903312b2dd66c36db
ahnafi <nafxd9896@gmail.com>
2023-03-19 22:00:59 +0700

    menambah fitur random kata animeh

--- js/main.js
+++ js/main.js
@@ -18,30 +18,46 @@ function jokes(){
 }
 function animekataBijak (param){
     // ide,masukan parameter dari btn terus ubah /api/getrandom dari parameter
+    $('#TampilData').html('');
     $.getJSON('https://katanime.vercel.app/api/'+param+'',function(i){
-        if (param == "getrandom"){
+    //    kondisi pertama
+    if (param == "getrandom"){
             let data = i['result'];
             data.forEach(e => {
+                let anim = e['anime']
+                let ind = e['indo']
+                let char = e['character']
+                let eng = e['english']
              $('#TampilData').append(`
-                <p>${e['indo']}</p>
-                <p>${e['anime']}</p>
-                <hr>
+             <p>
+             <em
+               >"${eng}"</em
+             >
+             <br><br>
+             <em
+               >"${ind}"</em
+             >
+           </p>
+           <p class="fw-semibold text">${char} - ${anim}</p>
+           <hr>
                 `)
             });
+            // 
         } else if (param == "getlistanime"){
             console.log(i)
         }
     })
 }
-// animekataBijak('getrandom')
 // animekataBijak('getlistanime')
-
-
 
 
 $('#btnJokes').on('click',function(){
     jokes();
 })
 $('#btnanime').on('click',function(){
-    
+    animekataBijak('getrandom');
+})
+$('#btnAnimeOnePiece').on('click',function(){
+    // animekataBijak('getbyanime?anime=naruto&page=1');
+    alert('fitur masih dikembangkan')
 })
